a72a57254088f60ae0d79e8fb8a0b5e3983ad876
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-11-23 21:18:35 -0500

    Revved

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Cushion.Mixfile do
      name: "cushion",
      source_url: "https://www.github.com/ryanbillingsley/cushion",
      description: description,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
